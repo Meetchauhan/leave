3014e06e56cf1fa754bf56e703b1d207de53e263
Meetchauhan <35761989+Meetchauhan@users.noreply.github.com>
2023-05-03 11:27:55 +0530

    first commit

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -14,7 +14,6 @@ export default function LeaveForm() {
       days: "",
       startDate: "",
       holidays: numOfDatePicker,
-      confirm_password: "",
       saturday: false,
       sunday: false,
     },
